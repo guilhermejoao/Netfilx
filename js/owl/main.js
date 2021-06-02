ef1075a67687b52869add51d5949981a9428cfca
joao.gui.spinola@gmail.com <waronline296@gmail.com>
2021-06-01 22:30:46 -0300

    first commit

---
+++ js/owl/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        4:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
